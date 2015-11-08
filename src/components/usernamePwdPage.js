e408a65a164283c5b4c806e4d166ed16d4783e98
Ivan Arandjelovic <ivan.arandjelovic@gmail.com>
2015-11-08 21:03:13 +0100

    step2 and done pages added

--- src/components/usernamePwdPage.js
+++ src/components/usernamePwdPage.js
@@ -2,14 +2,57 @@
 
 var React = require('react');
 var Router = require('react-router');
+var Step1Form = require('./step1form');
 var Link = Router.Link;
 
 var UsernamePwdPage = React.createClass({
+
+	mixins: [
+		Router.Navigation
+	],
+
+	getInitialState: function() {
+		return {
+			user: {
+				username: null,
+				password: null
+			},
+			errors: {
+				username: null,
+				password: null
+			},
+			dirty: false
+		};
+	},
+
+	setUserState: function(event) {
+		this.setState({dirty: true});
+		var field = event.target.name;
+		var value = event.target.value;
+		this.state.user[field] = value;
+		return this.setState({user: this.state.user});
+	},
+
+	proceedWithLogin: function(event) {
+		event.preventDefault();
+		// TODO: Validate
+
+		//Attempt Login
+
+
+		this.setState({dirty: false});
+
+		this.transitionTo('step2');
+	},
+
 	render: function() {
 		return (
 			<div className="jumbotron">
-				<h1>Login step 1</h1>
-				<p>TODO</p>
+				<Step1Form
+					user={this.state.user}
+					errors={this.state.errors}
+					onChange={this.setUserState}
+					onNext={this.proceedWithLogin}/>
 			</div>
 		);
 	}
--- src/routes.js
+++ src/routes.js
@@ -10,6 +10,8 @@ var Redirect = Router.Redirect;
 
 var routes = (
   <Route name="app" path="/" handler={require('./components/app')}>
+    <Route name="step2" handler={require('./components/step2page')} />
+      <Route name="loginDone" handler={require('./components/loginDonePage')} />
     <DefaultRoute handler={require('./components/usernamePwdPage')} />
     <NotFoundRoute handler={require('./components/notFoundPage')} />
   </Route>
